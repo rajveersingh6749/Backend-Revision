3b1a85b224f9c006f125f7c227ea68d9696d83d7
Rajveer Singh <777rajveersingh@gmail.com>
2025-07-24 06:02:28 +0530

    Re-add F3-npm-basics as regular folder

---
+++ F3-npm-basics/script.js
@@ -0,0 +1,16 @@
+// NPM understanding
+// installing and uninstalling 
+// understanding node_modules
+// dependencies and devDependencies
+// scripts - understanding default scripts PATH and custom scripts
+
+// Default scripts are // - start and // - test
+// We write something like npm start, npm test, becuase these are already present in operating system when we install node.js and package.json is created.
+
+// Custom scripts can be added in package.json under "scripts" section
+// Example "chacha": "node ./script.js"
+
+// it can be run using npm run <script-name>
+
+
+console.log("Hello, NPM!");

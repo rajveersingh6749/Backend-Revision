c8a5acdfb7171512fdc218868513d3c2b09597ef
Rajveer Singh <777rajveersingh@gmail.com>
2025-07-30 02:08:20 +0530

    CRUD operations with mongoose

---
+++ F9-CRUD-OperationsWithEJS/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const userModel = require('./models/user');
+
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+  res.render("index");
+});
+
+app.get('/read', async (req, res) => {
+  let users = await userModel.find()
+  res.render("read", {users});
+});
+
+app.get('/delete/:id', async (req, res) => {
+  let users = await userModel.findOneAndDelete({_id: req.params.id});
+  res.redirect('/read');
+});
+
+app.post('/create', async (req, res) => {
+  const { name, email, image } = req.body;
+
+  const userCreated = await userModel.create({
+    name,
+    email,
+    image
+  });
+
+  res.redirect('/read');
+})
+
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});

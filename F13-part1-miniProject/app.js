98b13bf1d3a1e164c3eb215cdb6167e5b9275e2f
Rajveer Singh <777rajveersingh@gmail.com>
2025-08-03 08:18:21 +0530

    second commit for creating post fields

--- F13-part1-miniProject/app.js
+++ F13-part1-miniProject/app.js
@@ -20,9 +20,24 @@ app.get('/login', function (req, res) {
   res.render("login");
 });
 
-app.get('/profile', isLoggedIn, function(req, res){
-  console.log(req.user)
-  res.render("login");
+app.get('/profile', isLoggedIn, async function(req, res){
+  let user = await userModel.findOne({email: req.user.email}).populate("posts");
+  res.render("profile", {user});
+});
+
+app.post('/post', isLoggedIn, async (req, res) => {
+  let user = await userModel.findOne({email: req.user.email});
+
+  let { content } = req.body;
+
+  let post = await postModel.create({
+    user: user._id,
+    content
+  });
+
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect("/profile");
 });
 
 app.post("/register", async (req, res) => {
@@ -60,7 +75,7 @@ app.post("/login", async (req, res) => {
     if(result){
       let token = jwt.sign({ email: email, userid: user._id }, "shhhh");
       res.cookie("token", token);
-      res.status(200).send("You can login!");
+      res.status(200).redirect("/profile");
     }
     else res.redirect("/login");
   })
@@ -71,8 +86,9 @@ app.get('/logout', (req, res) => {
   res.redirect('/login');
 });
 
+// this is called protected route
 function isLoggedIn(req, res, next){
-  if(req.cookies.token === "") res.send("You must be logged in");
+  if(req.cookies.token === "") res.redirect("/login");
   else{
     let data = jwt.verify(req.cookies.token, "shhhh")
     req.user = data;
--- F13-part1-miniProject/models/post.js
+++ F13-part1-miniProject/models/post.js
@@ -18,4 +18,4 @@ const postSchema = mongoose.Schema({
     ]
 });
 
-module.exports = mongoose.model("Post", postSchema);
+module.exports = mongoose.model("post", postSchema);

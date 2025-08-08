3c2551e67fcea2725e4d6020d7a555d1e0548f9f
Rajveer Singh <777rajveersingh@gmail.com>
2025-08-08 06:47:01 +0530

    learn multer

--- F13-part1-miniProject/app.js
+++ F13-part1-miniProject/app.js
@@ -6,15 +6,61 @@ const postModel = require("./models/post");
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+// const multer  = require('multer')
+// const upload = multer({ dest: 'uploads/' });
+const path = require("path");
+const upload = require('./config/multerconfig');
+const user = require("./models/user");
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")))
 app.use(cookieParser());
+
+
+// copied from npm multer and made some modifications
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, './public/images/uploads')
+//   },
+//   filename: function (req, file, cb) {
+//     crypto.randomBytes(12, function(err, bytes) {
+//       const fn = bytes.toString("hex") + path.extname(file.originalname);
+//       cb(null, fn)
+//     })
+//   }
+// })
+
+// const upload = multer({ storage: storage })
+// -------
+
+// Industry Standards: commented out all multer code and made a config folder
+
 
 app.get("/", function (req, res) {
   res.render("index");
 });
+
+app.get("/profile/upload", function (req, res) {
+  res.render("profileupload");
+});
+
+app.post("/upload", isLoggedIn, upload.single("image"), async function (req, res) {
+  let user = await userModel.findOne({email: req.user.email});
+  user.profilepic = req.file.filename;
+  await user.save();
+  res.redirect("/profile");
+});
+
+// app.get('/test', (req, res) => {
+//   res.render("test");
+// })
+
+// app.post('/upload', upload.single("image"), (req, res) => {
+//   console.log(req.file);
+// });
 
 app.get('/login', function (req, res) {
   res.render("login");
@@ -113,14 +159,15 @@ app.get('/logout', (req, res) => {
 
 // this is called protected route
 function isLoggedIn(req, res, next){
-  if(req.cookies.token === "") res.redirect("/login");
-  else{
-    let data = jwt.verify(req.cookies.token, "shhhh")
+  if(!req.cookies.token) return res.redirect("/login");
+  try {
+    let data = jwt.verify(req.cookies.token, "shhhh");
     req.user = data;
+    next();
+  } catch (err) {
+    return res.redirect("/login");
   }
-  next();
 }
-
 
 app.listen(3000, function (req, res) {
   console.log("This app is listening on 3000");
